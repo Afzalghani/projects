5bf91a4a26f1b541dcc6c973a96895c0ab10a949
Afzal <afzal.ghani@hexaviewtech.com>
2023-05-30 14:19:56 +0530

    Added the code for the formhandler

--- main.go
+++ main.go
@@ -1,6 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func main(){
-	
+func main() {
+
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileServer)
+
+	http.HandleFunc("/form", formHandler)
+
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8000\n")
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+     if r.URL.Path != "/hello"{
+		http.Error(w,"404 not found", http.StatusNotFound)
+		return
+	 }
+
+	 if r.Method!= "GET"{
+		http.Error(w,"Method not supported", http.StatusNotFound)
+		return
+	 }
+	 fmt.Fprintf(w, "hello")
+	 w.Write([]byte("This is me and how can I help you"))
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err!= nil{
+		fmt.Fprintf(w, "Parseforme() err %v", err)
+	}
+
+	fmt.Fprintf(w, "Post request has been successful")
+
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %s \n",name)
+	fmt.Fprintf(w, "Addess +%s \n", address)
+
+
 }
